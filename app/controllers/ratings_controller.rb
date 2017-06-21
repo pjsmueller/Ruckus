8ae27504a1bb79b4dcaf57cdcb25adce8187a48a
Jun Ota <Jun-Ota@users.noreply.github.com>
2017-06-21 16:27:04 -0700

    Add basic controllers.

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,50 @@
+class RatingsController < ApplicationController
+
+  def index
+    @rating = Rating.all
+  end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    if @rating.save
+      redirect_to @rating
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @rating = Rating.find(params[:id])
+  end
+
+  def edit
+    @rating = Rating.find(params[:id])
+  end
+
+  def update
+    @rating = Rating.find(params[:id])
+
+    if @rating.update(rating_params)
+      redirect_to @rating
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @rating = Rating.find(params[:id])
+    @rating.destroy
+
+    redirect_to ratings_path
+  end
+
+  private
+  def rating_params
+    params.require(:rating).permit(:user_id, :rateable_type, :rateable_id, :score)
+  end
+
+end
