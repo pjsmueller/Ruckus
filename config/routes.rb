9c56c2b4eb0b44940a77e4f916f734669919edfc
Jun Ota <Jun-Ota@users.noreply.github.com>
2017-06-22 12:26:27 -0700

    Merge confclict resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
 
 #users
-<<<<<<< HEAD
-  resources :users do
-    resources :reviews #?? Necessary?
-  end
-=======
+
   resources :users
->>>>>>> 639e36146d3c8eacf6bad5afa89d3ce0c63475df
 
 #comments
   resources :comments, except: [:show]
