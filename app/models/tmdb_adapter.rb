5e136423ac6da44cd6375ffbdf2ec8c9c6c51f5e
thisisDom <Dominique.Crowther@gmail.com>
2017-06-22 09:37:29 -0700

    adds methods for getting various api data

--- app/models/tmdb_adapter.rb
+++ app/models/tmdb_adapter.rb
@@ -7,7 +7,19 @@ class TMDBAdapter
   def self.get_movie_details(api_id)
       movie_details = Tmdb::Movie.detail(api_id)
       movie_details["directors"] = Tmdb::Movie.directors(api_id)
-      movie_details["api_id"] = api_id
+      movie_details["cast"] = Tmdb::Movie.cast(api_id)
       movie_details
+  end
+
+  def self.movies_by_genre(genre_id)
+    Tmdb::Genre.movies(genre_id)
+  end
+
+  def self.get_genres
+    Tmdb::Genre.movie_list.map { |genre| genre["name"] }
+  end
+
+  def self.now_playing
+    Tmdb::Movie.top_rated
   end
 end
