cd8b9d076900265e0d9baa7e20dce698827a0b0a
[01;31m[K"name"[m[K: "Nick <[01;31m[K"email"[m[K: null>
2017-06-23 16:45:28 -0700

    fix reviews display and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ require 'faker'
 3.times do
   User.create(username: Faker::Internet.user_name, f_name: Faker::Name.first_name, l_name: Faker::Name.last_name, password: "pass")
 end
-movies = Movie.now_playing
-movies.each do |movie|
-  Movie.create(api_id: movie.id )
-end
-6.times do
+# movies = Movie.now_playing
+# movies.each do |movie|
+#   Movie.create(api_id: movie.id )
+# end
+100.times do
   Review.create(title: Faker::Hipster.word, body: Faker::Hacker.say_something_smart, user_id: User.all.sample.id, movie_id: Movie.all.sample.id)
 end
-20.times do
+150.times do
   Comment.create(body: Faker::Hacker.say_something_smart, user_id: User.all.sample.id, review_id: Review.all.sample.id)
 end
 
 types = ["Review", "Comment", "Movie"]
-30.times do
+300.times do
 ids = [Review.all.sample.id, Movie.all.sample.id, Comment.all.sample.id]
   picked_type = types.sample
   if picked_type == "Review"
