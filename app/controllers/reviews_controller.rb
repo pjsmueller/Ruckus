491fc919ac514b0c8fff97952475c9c433c434e6
[01;31m[K"name"[m[K: "Nick <[01;31m[K"email"[m[K: null>
2017-06-23 12:43:31 -0700

    add the logic for edit and delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,25 +6,20 @@ def index
 end
 
 def new
-  # @movie = Movie.get_by_id(params[:movie_id])
   @movie = Movie.find_by(api_id: params[:movie_id])
   if !@movie
    @movie = Movie.create({api_id: params[:movie_id]})
   end
-  p @movie
-  p "@" * 20
+
   @review = Review.new
 end
 
 def create
   @movie = Movie.find(params[:movie_id])
   @review = Review.new(reviews_params)
-  p params
-  p "@" * 20
-  p @review
   if @review.save
-    p "@review" * 20
-    redirect_to "movies/#{@movie.id}/reviews/#{@review.id}"
+    @movie = Movie.get_by_id(@movie.api_id.to_s)
+    redirect_to "/movies/#{@movie.id}"
   else
     render 'new'
   end
@@ -32,28 +27,32 @@ def create
 end
 
 def show
+  @movie = Movie.get_by_id(params[:movie_id])
   @review = Review.find(params[:id])
 end
 
 def edit
+  @movie = Movie.find_by(api_id: params[:movie_id])
   @review = Review.find(params[:id])
 end
 
 def update
   @review = Review.find(params[:id])
-
+  @movie = Movie.find_by(api_id: params[:movie_id])
   if @review.update(reviews_params)
-    redirect_to @review
+    @movie = Movie.get_by_id(@movie.api_id.to_s)
+    redirect_to "/movies/#{@movie.id}"
   else
     render 'edit'
   end
 end
 
 def destroy
+  @movie = Movie.find_by(api_id: params[:movie_id])
   @review = Review.find(params[:id])
   @review.destroy
 
-  redirect_to reviews_path
+  redirect_to "/movies/#{@movie.api_id}"
 end
 
 private
