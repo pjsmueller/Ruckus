e578febe6a695332f5e3249d6bde748c28c96902
thisisDom <Dominique.Crowther@gmail.com>
2017-06-23 14:09:51 -0700

    fixes errors in star rating and application error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,11 @@ module ApplicationHelper
   def average_ratings(ratings_array)
     average_rating = ratings_array.reduce(0) do |sum, rating|
       sum + rating.score
-    if ratings_array.length > 0
-       avrage_rating = avrage_rating / ratings_array.length
-    else
-      0
+      if ratings_array.length > 0
+         avrage_rating = avrage_rating / ratings_array.length
+      else
+        0
+      end
     end
   end
 end
